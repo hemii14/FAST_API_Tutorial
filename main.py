4faa93a08dd1ea0e5ad8b0f4b2990f0c7a793761
Hemapriya C <136780101+hemii14@users.noreply.github.com>
2025-06-16 16:29:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 from fastapi import FastAPI
-from blog import  models
-from blog.database import engine
-from blog.routers import blog, user, authentication
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
 
-app.include_router(authentication.router)
-app.include_router(blog.router)
-app.include_router(user.router)
-
-
-# def get_db():
-#     db=SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
