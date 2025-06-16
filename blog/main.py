9c77a2afbb08520b5bb2fa0f2fe12e215ecb69fe
Hemapriya C <136780101+hemii14@users.noreply.github.com>
2025-06-16 16:39:04 +0530

    Add files via upload

---
+++ blog/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from blog import  models
+from blog.database import engine
+from blog.routers import blog, user, authentication
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
+
+
+# def get_db():
+#     db=SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
